d341b73a9ac9c6c25312fe01e49cd394058cd95b
Ben Swartzlander <ben@swartzlander.org>
2017-01-30 10:09:31 -0500

    Support testing on Fedora
    Try both flavors of the kvm binary, and test with minimal RAM.
    
    Change-Id: I58e0024be184d55d32f21d4b16733df909fd7844

--- test-image.sh
+++ test-image.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+KVM=kvm
+if ! which $KVM 2> /dev/null ; then
+    KVM=qemu-kvm
+fi
+
 qemu-img create -f qcow2 -b client.qcow2 client-test.qcow2
-kvm -m 512 -monitor none -nographic \
+$KVM -m 64 -monitor none -nographic \
     -drive file=client-test.qcow2,if=virtio,format=qcow2 \
     -netdev user,id=net0 -device virtio-net-pci,netdev=net0
 rm client-test.qcow2
